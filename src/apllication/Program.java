93a2eeeec834a5e3d23ad7f9b1a1c9d3849aaaa7
Rodnei Gustavo <rodneigstv@hotmail.com>
2022-08-20 17:03:01 -0300

    Seller entity class

--- src/apllication/Program.java
+++ src/apllication/Program.java
@@ -1,12 +1,18 @@
 package apllication;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
+		
+		Seller obj1 = new Seller(21, "BOB", "bob@gmail.com", new Date(), 3000.0, obj);
+		System.out.println(obj1);
 	}
 
 }
