51ffbc723b8ec6117120b48d106a952eadaed6f8
rodneigstv <rodneigstv@hotmail.com>
2022-08-23 12:22:49 -0300

    findAll implementation

--- src/apllication/Program.java
+++ src/apllication/Program.java
@@ -25,5 +25,12 @@ public class Program {
 			System.out.println(obj);
 		}
 		
+		System.out.println("\n===== TEST 3: seller findByAll =====");
+		list = sellerDao.findAll();
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
+		
+		
 	}
 }
